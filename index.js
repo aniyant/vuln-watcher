cb08c17b13d134ca2d254f56b1a5e3e1ff75d033
Aniyant <aniyant@Aniyants-MacBook-Air.local>
2026-08-02 21:44:16 +0530

    added few functions

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function add(a, b) {
     return a + b;
 }
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
 console.log(add(4, 3));
 console.log(add(5,9));
+console.log("subtract: " + subtract(10, 5));
+console.log("multiply: " + multiply(4, 5));
