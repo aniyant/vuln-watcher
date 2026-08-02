7e4e9bd7f808e95f7913a3f2c575f1d777221584
Aniyant <aniyant@Aniyants-MacBook-Air.local>
2026-08-02 21:48:53 +0530

    added more

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ function subtract(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
 
 console.log(add(4, 3));
 console.log(add(5,9));
 console.log("subtract: " + subtract(10, 5));
 console.log("multiply: " + multiply(4, 5));
+console.log("divide: " + divide(20, 4));
